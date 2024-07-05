9f5e47aefd33ba990164c3408877a04659533aaf
Aykut <gunesaykut92@gmail.com>
2024-07-05 23:43:16 +0300

    Initial commit

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Data;
+using WebApplication2.Models;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication2.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UsersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Users.ToListAsync());
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(user);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user);
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserID { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string UserName { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(100)]
+        public string UserEmail { get; set; } = string.Empty;
+    }
+}
